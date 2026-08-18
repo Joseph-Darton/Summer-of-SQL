c4eab3b1da0a8974a741e72a093e62f5253b01d2
Joseph Darton <94755464+Joseph-Darton@users.noreply.github.com>
2026-08-18 17:25:47 +0100

    Update b_Runner_and_Customer_Experience.sql

--- 3. Pizza Runner/b_Runner_and_Customer_Experience.sql
+++ 3. Pizza Runner/b_Runner_and_Customer_Experience.sql
@@ -1,4 +1,18 @@
 -- 1. How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+with runner_signups as (
+select
+runner_id,
+registration_date,
+date_trunc('week', registration_date) + interval '4' day as start_of_week
+from runners
+)
+
+select
+count(runner_id) as runners,
+start_of_week
+from runner_signups
+group by start_of_week
+order by start_of_week
 -- 2. What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
 -- 3. Is there any relationship between the number of pizzas and how long the order takes to prepare?
 -- 4. What was the average distance travelled for each customer?
